a6afd8c7f35aff2460a23b7db15edb53af7396d9
elfsprin <elfsprin@gmail.com>
2015-04-26 14:27:36 -0500

    updates

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Get-Clean-project-2
-Course Project 2
+# run_analysis.R script
+
+## Before using this script
+* This script assumes that you have downloaded the zip file from https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip and that you have unzipped it and placed its parent folder, UCI HAR Dataset, along with all of its contents in your working directory.  
+  
+* This script assumes that you have installed the package reshape2.  
+  
+## What this script does
+
+1. Reads in the Subject data for the test and train data.  
+2. Reads in the Activity data for the test and train data.  
+        + Replaces the numeric Activity labels found in the 'y_test.txt' and 'y_train.txt' files with the descriptive names for Activities found in the 'activity_labels.txt' file.  
+3. Creates a list of descriptive Measurement names for any measurements providing a  mean or standard deviation.  
+        + The descriptive names are based on the information found in the 'features.txt' and 'features_info.txt' and 'README.txt' files included in the downloaded data.  
+4. Reads in the Measurement data for the test and train data, saving the full data to  'testSet' and 'trainSet'.  
+5. Creates subsets of the Measurement data, 'testSet1' and 'trainSet1', which include only Measurements where the mean or standard deviation is provided.  
+        + These columns from 'testSet' and 'trainSet' are included in 'testSet1' and 'trainSet1' respectively: 1:6, 41:46, 81:86, 121:126, 161:166, 201:202, 214:215, 227:228, 240:241, 253:254, 266:271, 294:296, 345:350, 373:375, 424:429, 452:454, 503:504, 513, 516:517, 526, 529:530, 539, 542:543, 552  
+6. Applies the descriptive Measurement names to the columns in the Measurement subsets.  
+7. Creates a test dataframe 'testDF' and a train dataframe 'trainDF' by column binding their respective Subject, Activity, and Measurement data.  
+8. Creates a single comprehensive dataframe 'fullDF' by row binding the 'testDF' and 'trainDF' dataframes.  
+9. Melts 'fullDF' using Subject and Activity as identity variables, creating 'fullDFmelt'.  
+10. Aggregates the 'fullDFmelt' Value field by Subject, Activity and Measurement using the mean function.  This produces this fullDFmeans dataframe, which provides the Mean of each Measurement for each Subject and Activity.
--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,36 @@
-# Read in the test and train activity labels
-        testLabels <- read.table("./UCI HAR Dataset/test/y_test.txt", 
-                quote="\"")
-        trainLabels <- read.table("./UCI HAR Dataset/train/y_train.txt", 
-                quote="\"")
+# Read in test and train subjects and apply a column label
+subjectTest <- read.table("./UCI HAR Dataset/test/subject_test.txt", 
+                          quote="\"",col.names=c("Subject"))
 
-# Create decriptive names for columns of interest
-        features1 <- c("TimeBodyAccelerationMeanOfX",
+subjectTrain <- read.table("./UCI HAR Dataset/train/subject_train.txt", 
+                           quote="\"",col.names=c("Subject"))
+
+# Read in the test and train activity labels and apply a column label
+        testLabels <- read.table("./UCI HAR Dataset/test/y_test.txt", 
+                quote="\"",col.names=c("Activity"))
+
+        trainLabels <- read.table("./UCI HAR Dataset/train/y_train.txt", 
+                quote="\"",col.names=c("Activity"))
+
+# Assign descriptive names to the test and train activities. These replace 
+# operations are based on the activity_labels.txt file found in the UCI HAR 
+# Dataset folder in the data.
+        testLabels$Activity[testLabels$Activity == 1] <- "WALKING"
+        testLabels$Activity[testLabels$Activity == 2] <- "WALKING_UPSTAIRS"
+        testLabels$Activity[testLabels$Activity == 3] <- "WALKING_DOWNSTAIRS"
+        testLabels$Activity[testLabels$Activity == 4] <- "SITTING"
+        testLabels$Activity[testLabels$Activity == 5] <- "STANDING"
+        testLabels$Activity[testLabels$Activity == 6] <- "LAYING"
+
+        trainLabels$Activity[trainLabels$Activity == 1] <- "WALKING"
+        trainLabels$Activity[trainLabels$Activity == 2] <- "WALKING_UPSTAIRS"
+        trainLabels$Activity[trainLabels$Activity == 3] <- "WALKING_DOWNSTAIRS"
+        trainLabels$Activity[trainLabels$Activity == 4] <- "SITTING"
+        trainLabels$Activity[trainLabels$Activity == 5] <- "STANDING"
+        trainLabels$Activity[trainLabels$Activity == 6] <- "LAYING"
+
+# Create decriptive names for columns of interest from the test and train sets
+        features <- c("TimeBodyAccelerationMeanOfX",
                 "TimeBodyAccelerationMeanOfY","TimeBodyAccelerationMeanOfZ",
                 "TimeBodyAccelerationStandardDeviationOfX",
                 "TimeBodyAccelerationStandardDeviationOfY",
@@ -48,71 +73,67 @@
                 "FrequencyBodyAccelerationStandardDeviationOfX",
                 "FrequencyBodyAccelerationStandardDeviationOfY",
                 "FrequencyBodyAccelerationStandardDeviationOfZ",
+                "FrequencyBodyAccelerationMeanFrequencyOfX",
+                "FrequencyBodyAccelerationMeanFrequencyOfY",
+                "FrequencyBodyAccelerationMeanFrequencyOfZ",
                 "FrequencyBodyAccelerationJerkMeanOfX",
                 "FrequencyBodyAccelerationJerkMeanOfY",
                 "FrequencyBodyAccelerationJerkMeanOfZ",
                 "FrequencyBodyAccelerationJerkStandardDeviationOfX",
                 "FrequencyBodyAccelerationJerkStandardDeviationOfY",
                 "FrequencyBodyAccelerationJerkStandardDeviationOfZ",
+                "FrequencyBodyAccelerationJerkMeanFrequencyOfX",
+                "FrequencyBodyAccelerationJerkMeanFrequencyOfY",
+                "FrequencyBodyAccelerationJerkMeanFrequencyOfZ",
                 "FrequencyBodyGyroscopeMeanOfX","FrequencyBodyGyroscopeMeanOfY",
                 "FrequencyBodyGyroscopeMeanOfZ",
                 "FrequencyBodyGyroscopeStandardDeviationOfX",
                 "FrequencyBodyGyroscopeStandardDeviationOfY",
                 "FrequencyBodyGyroscopeStandardDeviationOfZ",
+                "FrequencyBodyGyroscopeMeanFrequencyOfX",
+                "FrequencyBodyGyroscopeMeanFrequencyOfY",
+                "FrequencyBodyGyroscopeMeanFrequencyOfZ",
                 "FrequencyBodyAccelerationMagnitudeMean",
                 "FrequencyBodyAccelerationMagnitudeStandardDeviation",
+                "FrequencyBodyAccelerationMagnitudeMeanFrequency",
                 "FrequencyBodyAccelerationJerkMagnitudeMean",
                 "FrequencyBodyAccelerationJerkMagnitudeStandardDeviation",
+                "FrequencyBodyAccelerationJerkMagnitudeMeanFrequency",
                 "FrequencyBodyGyroscopeMagnitudeMean",
                 "FrequencyBodyGyroscopeMagnitudeStandardDeviation",
+                "FrequencyBodyGyroscopeMagnitudeMeanFrequency",
                 "FrequencyBodyGyroscopeJerkMagnitudeMean",
-                "FrequencyBodyGyroscopeJerkMagnitudeStandardDeviation")
+                "FrequencyBodyGyroscopeJerkMagnitudeStandardDeviation",
+                "FrequencyBodyGyroscopeJerkMagnitudeMeanFrequency")
         
-# Read in test and train sets, drop unneeded columns, apply descriptive names
+# Read in test and train sets, pull out the columns related to mean or standard
+# deviation, apply descriptive names to those columns
         testSet <- read.table("./UCI HAR Dataset/test/X_test.txt", quote="\"")
+        
         trainSet <- read.table("./UCI HAR Dataset/train/X_train.txt", 
                 quote="\"")
         
         testSet1 <- testSet[c(1:6,41:46,81:86,121:126,161:166,201:202,214:215,
-                227:228,240:241,253:254,266:271,345:350,424:429,503:504,516:517,
-                529:530,542:543)]
+                227:228,240:241,253:254,266:271,294:296,345:350,373:375,424:429,
+                452:454,503:504,513,516:517,526,529:530,539,542:543,552)]
+
         trainSet1 <- trainSet[c(1:6,41:46,81:86,121:126,161:166,201:202,214:215,
-                227:228,240:241,253:254,266:271,345:350,424:429,503:504,516:517,
-                529:530,542:543)]
+                227:228,240:241,253:254,266:271,294:296,345:350,373:375,424:429,
+                452:454,503:504,513,516:517,526,529:530,539,542:543,552)]
 
-        names(testSet1) <- features1
-        names(trainSet1) <- features1
-        
-# Read in test and train subjects
-        subjectTest <- read.table("./UCI HAR Dataset/test/subject_test.txt", 
-                quote="\"", col.names=c("Subject"))
-        subjectTrain <- read.table("./UCI HAR Dataset/train/subject_train.txt", 
-                quote="\"", col.names=c("Subject"))
+        names(testSet1) <- features
 
-# Assign descriptive names to the test and train activities and apply column 
-# name
-        testLabels$V1[testLabels$V1 == 1] <- "WALKING"
-        testLabels$V1[testLabels$V1 == 2] <- "WALKING_UPSTAIRS"
-        testLabels$V1[testLabels$V1 == 3] <- "WALKING_DOWNSTAIRS"
-        testLabels$V1[testLabels$V1 == 4] <- "SITTING"
-        testLabels$V1[testLabels$V1 == 5] <- "STANDING"
-        testLabels$V1[testLabels$V1 == 6] <- "LAYING"
-        trainLabels$V1[trainLabels$V1 == 1] <- "WALKING"
-        trainLabels$V1[trainLabels$V1 == 2] <- "WALKING_UPSTAIRS"
-        trainLabels$V1[trainLabels$V1 == 3] <- "WALKING_DOWNSTAIRS"
-        trainLabels$V1[trainLabels$V1 == 4] <- "SITTING"
-        trainLabels$V1[trainLabels$V1 == 5] <- "STANDING"
-        trainLabels$V1[trainLabels$V1 == 6] <- "LAYING"
-        
-        names(testLabels) <- c("Activity")
-        names(trainLabels) <- c("Activity")
+        names(trainSet1) <- features
 
-# Create test and train dataframes, do not rename columns
+# Create test and train dataframes by cbinding the subject, activity, and 
+# measurement dataframes for each; do not rename columns
         testDF <- cbind(subjectTest,testLabels,testSet1,deparse.level=0)
+
         trainDF <- cbind(subjectTrain,trainLabels,trainSet1,
                 deparse.level=0)
 
-# Merge test and train dataframes, do not rename columns
+# Merge test and train dataframes via rbind; do not rename columns. fullDF is 
+# the tidy dataset required in step 4 of this course project.
         fullDF<-rbind(testDF,trainDF,deparse.level=0)
 
 # Melt fullDF to prepare for aggregating
@@ -120,8 +141,10 @@
         fullDFmelt <- melt(fullDF,id=c("Subject","Activity"))
         names(fullDFmelt) <- c("Subject","Activity","Measurement","Value")
 
-# Aggregate the Value based on the other three columns, grouped, and rename
-# the columns
+# Aggregate the Value field based on Subject, Activity and Measurement using 
+# the mean function; reapply column names. fullDFmeans is the tidy dataset
+# required in step 5 of this course project.
+
         fullDFmeans <- aggregate(fullDFmelt$Value,list(fullDFmelt$Subject,
                 fullDFmelt$Activity,fullDFmelt$Measurement),mean)
         names(fullDFmeans) <- c("Subject","Activity","Measurement","Mean")
